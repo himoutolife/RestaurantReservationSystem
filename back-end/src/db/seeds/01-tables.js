ed1d7c35714f45b0c3b5cd21bc041a1478c10412
Natalie Natalino <70120038+himoutolife@users.noreply.github.com>
2021-11-18 13:03:53 -0800

    Add files via upload

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -2,8 +2,6 @@ const tables = require("./01-tables.json");
 
 exports.seed = function(knex) {
   return knex
-  // Deletes ALL existing entries
     .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-    // Inserts seed entries
     .then(() => knex("tables").insert(tables));
 };
